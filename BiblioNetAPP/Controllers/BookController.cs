885debc8233a772646d4e3bcf69a53b3c9d40226
JulyMorales22 <jamorales859@misena.edu.co>
2022-03-22 10:23:47 -0500

    conexion a la base de datos y registro de book

--- BiblioNetAPP/Controllers/BookController.cs
+++ BiblioNetAPP/Controllers/BookController.cs
@@ -1,21 +1,32 @@
 ﻿using BiblioNetAPP.Models;
+using BiblioNetAPP.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BiblioNetAPP.Controllers
 {
     public class BookController : Controller
     {
+        private readonly IRepositorieBook repositorieBook;
+
+        //constructor?
+        public BookController(IRepositorieBook repositorieBook)
+        {
+            this.repositorieBook = repositorieBook;
+        }
         public IActionResult Create()
         {
             return View();
         }
         [HttpPost]
-        public IActionResult Create(Book book)
+        public IActionResult Create(Book book) //la variable se debe llamar igual que el modelo pero empezando en minuscula 
         {
             if (!ModelState.IsValid)
             {
                 return View(book);
             }
+            book.AuthorId = 1;
+            //book.Price = 25000;
+            repositorieBook.Create(book);
             return View();
         }
     }
---
+++ BiblioNetAPP/Services/RepositorieBook.cs
@@ -0,0 +1,28 @@
+﻿using BiblioNetAPP.Models;
+using Dapper;
+using Microsoft.Data.SqlClient;
+
+namespace BiblioNetAPP.Services
+{
+    public interface IRepositorieBook
+    {
+        void Create(Book book);
+    }
+    public class RepositorieBook : IRepositorieBook
+    {
+        private readonly string connectionString; 
+        public RepositorieBook(IConfiguration configuration) //ya no me acuerdo :'v
+        {
+            connectionString = configuration.GetConnectionString("DefaultConnection");
+
+        }
+        public void Create(Book book)
+        {
+            using var connection = new SqlConnection(connectionString);
+            var id = connection.QuerySingle<int>($@"INSERT INTO Book
+                                                (BookName, AuthorId, Price)
+                                                VALUES (@BookName, @AuthorId, @Price); SELECT SCOPE_IDENTITY();", book);
+        book.Id = id;
+        }
+    }
+}
